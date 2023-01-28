53c95668d077ed6d6084e0acf0f9327ada92dd3f
Markeljan Sokoli <Markeljan19@gmail.com>
2023-01-28 18:04:21 -0500

    cards

--- pages/heroes.tsx
+++ pages/heroes.tsx
@@ -1,4 +1,6 @@
-import { Stack, Typography } from "@mui/material";
+import { CustomConnect } from "@/components/CustomConnect";
+import { Box, Card, CardContent, CardMedia, Stack, Typography } from "@mui/material";
+import Image from "next/image";
 
 export default function Heroes() {
   return (
@@ -30,6 +32,33 @@ export default function Heroes() {
           The Dungeon of Souls
         </Typography>
       </Stack>
+
+      <Box>
+        <Card className="pixel-borders pixel-borders--2 " sx={{ p: 6 }}>
+          <CardMedia>
+            <Image src="/assets/dungeon-1.png" height={250} width={250} alt="temp" />
+          </CardMedia>
+          <CardContent>
+            <Typography variant="h4">Dungeon 1</Typography>
+            <CardContent>Risk: 1</CardContent>
+            <CardContent>Reward: 1</CardContent>
+            <CardContent>Description: heheh</CardContent>
+          </CardContent>
+        </Card>
+        <Card className="pixel-borders pixel-borders--2 " sx={{ p: 6 }}>
+          <CardMedia>
+            <Image src="/assets/dungeon-1.png" height={250} width={250} alt="temp" />
+          </CardMedia>
+          <CardContent>
+            <Typography variant="h4">Dungeon 1</Typography>
+            <CardContent>Risk: 1</CardContent>
+            <CardContent>Reward: 1</CardContent>
+            <CardContent>Description: heheh</CardContent>
+          </CardContent>
+        </Card>
+      </Box>
+
+      <CustomConnect />
     </Stack>
   );
 }
