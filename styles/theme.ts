f4dbc9bd42f4aef12c8f07ad9796999add9e8ff9
Alex Biet <alexandrubiet@gmail.com>
2023-02-05 22:31:09 +0100

    Update theme.ts

--- styles/theme.ts
+++ styles/theme.ts
@@ -35,7 +35,7 @@ export const theme = createTheme({
             borderImageSlice: "4",
             borderImageWidth: "2",
             borderImageOutset: "0",
-            borderImageSource: "url(data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' width='12' height='12'><path d='M2 2h2v2H2zM4 0h2v2H4zM10 4h2v2h-2zM0 4h2v2H0zM6 0h2v2H6zM8 2h2v2H8zM8 8h2v2H8zM6 10h2v2H6zM0 6h2v2H0zM10 6h2v2h-2zM4 10h2v2H4zM2 8h2v2H2z' fill='%23000' /></svg>)",
+            borderImageSource: 'url("data:image/svg+xml,<svg xmlns=\'http://www.w3.org/2000/svg\' width=\'12\' height=\'12\'><path d=\'M2 2h2v2H2zM4 0h2v2H4zM10 4h2v2h-2zM0 4h2v2H0zM6 0h2v2H6zM8 2h2v2H8zM8 8h2v2H8zM6 10h2v2H6zM0 6h2v2H0zM10 6h2v2h-2zM4 10h2v2H4zM2 8h2v2H2z\' fill=\'%23000\' /></svg>")',
             position: "relative",
             display: "inline-block",
             margin: "0 15px 15px 0",
